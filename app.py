5e10f4ce37febf70ff052c069c69bbe0ddf3486c
JoaoRobert0 <devjoaoroberto@gmail.com>
2025-03-06 13:22:34 -0300

    Add CRUD feature for tasks

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -11,9 +11,40 @@ class Task(db.Model):
 
 @app.route("/")
 def index():
-    tasks = ["Task 1", "Task 2", "Task 3"]
+    tasks = Task.query.all()
     return render_template('index.html', tasks = tasks)
 
+@app.route('/task/create', methods=["POST"])
+def create_task():
+    description = request.form['description']
+
+    if Task.query.filter_by(description = description).first() != None:
+        return "Error: Task doesnt exist", 404
+
+    new_task = Task(description = description)
+    db.session.add(new_task)
+    db.session.commit()
+    return redirect('/')
+
+@app.route('/task/delete/<int:task_id>', methods=["POST"])
+def delete_task(task_id):
+    task = Task.query.get(task_id)
+
+    if task:
+        db.session.delete(task)
+        db.session.commit()
+    return redirect('/')
+
+@app.route('/task/update/<int:task_id>', methods=["POST"])
+def update_task(task_id):
+    task = Task.query.get(task_id)
+
+    if task:
+        task.description = request.form['description']
+        db.session.commit()
+
+    return redirect("/")
+    
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
