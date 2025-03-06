e0af32c8da612186604adc2d960c7888d468ea79
JoaoRobert0 <devjoaoroberto@gmail.com>
2025-03-06 12:07:35 -0300

    Create connection to database

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app) # Instance of SQLAlchemy
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    description = db.Column(db.String(100), unique = True, nullable = False)
 
 @app.route("/")
 def index():
@@ -8,4 +15,6 @@ def index():
     return render_template('index.html', tasks = tasks)
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True, port="8000")
