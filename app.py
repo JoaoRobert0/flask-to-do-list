a7650331fa3025433e2a71753c04e0890e635b72
JoaoRobert0 <devjoaoroberto@gmail.com>
2025-03-06 11:27:19 -0300

    Render template and using Jinja

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello World"
+    tasks = ["Task 1", "Task 2", "Task 3"]
+    return render_template('index.html', tasks = tasks)
 
 if __name__ == "__main__":
     app.run(debug=True, port="8000")
